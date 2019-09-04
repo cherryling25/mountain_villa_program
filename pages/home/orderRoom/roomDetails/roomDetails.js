e69e3dde7030c5511a7f3131d38e415b91029069
Cherry <cherryling25@outlook.com>
2019-09-04 23:29:26 +0800

    roomDetails

---
+++ pages/home/orderRoom/roomDetails/roomDetails.js
@@ -0,0 +1,74 @@
+// pages/home/orderRoom/roomDetails/roomDetails.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    imgUrls: [
+      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640',
+      '../../../../images/1.png',
+      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 500
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
