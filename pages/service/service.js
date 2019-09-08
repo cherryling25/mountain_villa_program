b45cc96f0cc14d5aa257100224042ab8e8a52eb0
Cherry <cherryling25@outlook.com>
2019-09-08 10:41:26 +0800

    service

--- pages/service/service.js
+++ pages/service/service.js
@@ -1,5 +1,4 @@
-// pages/home/home.js
-
+// pages/service/service.js
 Page({
   //视频加载
   onReady: function (res) {
@@ -7,20 +6,19 @@ Page({
   },
 
   data: {
-    imgurl: 
+    imgUrls: [
+      '../../images/tu.png',
+      '../../images/1.png',
       'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
-    ,
-    
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 500,
+
     //视频路径
     src: "http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400"
   },
-  /**
-   * Lifecycle function--Called when page load
-   */
-  onLoad: function (options) {
-
-  },
-
   /**
    * Lifecycle function--Called when page is initially rendered
    */
